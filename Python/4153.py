3bf43547abfc68b82e01bdb6d42926ddf5065433
Srevil <defi622@naver.com>
2024-12-03 22:36:09 +0900

    24.12.03

---
+++ Python/4153.py
@@ -0,0 +1,8 @@
+a, b, c = sorted(map(int, input().split()))
+
+while a !=0 and b!=0 and c!=0:
+    if a**2 + b**2 == c**2:
+        print('right')
+    else:
+        print('wrong')
+    a, b, c = sorted(map(int, input().split()))
