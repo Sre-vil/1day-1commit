0282ac3de62ffecee1beabe2100dc466fa1c0474
Srevil <defi622@naver.com>
2024-12-07 01:03:41 +0900

    24.12.06

---
+++ Python/12.08_retry/2231.py
@@ -0,0 +1,11 @@
+n = int(input())
+res = 0
+
+for i in range(1, n+1):
+    num = list(map(int, str(i)))
+    res = sum(num) + i
+    if res == n:
+        print(i)
+        break
+    if i==n:
+        print(0)
